2e619552d964f0a5bc10d27a17d267cadaf1ce4a
Rushikesh Taralkar <rushikeshtaralkar07@gmail.com>
2025-01-19 09:37:40 +0000

    container config done

---
+++ 01_class/01_variable.js
@@ -0,0 +1,18 @@
+const accountId = 1234
+let accountEmail = "rushikesh12@gmail.com"
+var accountpassword = 212121
+accountcity = "Pune"
+
+/*
+cant use the var
+because some issues are create like a bloak scope and functional scope
+*/
+
+
+// accountId = 2 // not allowed because is const value
+
+// accountEmail="rst21@gmail.com"
+// accountpassword = "5689"
+// accountcity = "Mumbai"
+
+console.table([accountId,accountEmail,accountpassword,accountcity])
---
+++ 01_class/02_datatype.js
@@ -0,0 +1,13 @@
+console.log(3+3)
+console.log("Rushikesh")
+
+let name ="Rushi"
+let age=18
+let IsLogin = "false"
+
+// number => 2 power of 53 
+// bigint
+// string => ""
+// boolean => True/False
+// null => standloan value
+// undefined
